6d39f88a759e7712f10eb1d00f7a3d8a9c5ed4d3
Miracle <2893496202@qq.com>
2024-04-08 21:04:11 +0800

    feat: first initial

---
+++ src/test/java/com/miracle/api/MiracleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
