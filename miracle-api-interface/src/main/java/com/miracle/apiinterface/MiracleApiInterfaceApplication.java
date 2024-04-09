19e9d7d1cd3f237dfb0328a869581d77b3ec5b81
Miracle <2893496202@qq.com>
2024-04-09 20:33:16 +0800

    feat: Add analog interface project

---
+++ miracle-api-interface/src/main/java/com/miracle/apiinterface/MiracleApiInterfaceApplication.java
@@ -0,0 +1,17 @@
+package com.miracle.apiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author miracl
+ * @description 启动类
+ */
+@SpringBootApplication
+public class MiracleApiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiracleApiInterfaceApplication.class, args);
+    }
+
+}
---
+++ miracle-api-interface/src/main/java/com/miracle/apiinterface/aop/InvokeCountAOP.java
@@ -0,0 +1,9 @@
+package com.miracle.apiinterface.aop;
+
+/**
+ * @author dargon
+ * @create 2024-04-09
+ * 统计接口调用次数
+ */
+public class InvokeCountAOP {
+}
